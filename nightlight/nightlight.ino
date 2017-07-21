6bb45c811b5fe83d962944bb91a9b183394b9dac
Rory Zweistra <rory@ryuu.nl>
2017-07-21 20:00:34 +0200

    Update nightlight.ino
    Started move to millis. Not done yet because of bedtime and the need for his light to work.

--- nightlight/nightlight.ino
+++ nightlight/nightlight.ino
@@ -8,26 +8,30 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 
 // Set constants.
 // Set LED pins.
-const int LED_RED           = 13;
-const int LED_YELLOW        = 12;
-const int LED_GREEN         = 8;
-const int DELAY_TIME        = 10000;
+const int LED_RED             = 13;
+const int LED_YELLOW          = 12;
+const int LED_GREEN           = 8;
+const int DELAY_TIME          = 10000;
 
 // Set RTC power pins.
-const char RTC_GROUND        = A2;
-const char RTC_POWER         = A3;
+const char RTC_GROUND         = A2;
+const char RTC_POWER          = A3;
 
 // Set variables.
 // Set standard output.
-char output_red             = LOW;
-char output_yellow          = LOW;
-char output_green           = LOW;
+char output_red               = LOW;
+char output_yellow            = LOW;
+char output_green             = LOW;
 
 // Set previous ouput.
-char previous_output_red    = output_red;
-char previous_output_yellow = output_yellow;
-char previous_output_green  = output_green;
+char previous_output_red      = output_red;
+char previous_output_yellow   = output_yellow;
+char previous_output_green    = output_green;
 
+// Set timing/time variables.
+int query_datetime            = 1;
+int rtc_query_timeout         = 86400;
+unsigned long previous_millis = 0;
 DateTime now;
 
 void setup () {
@@ -52,8 +56,6 @@ void setup () {
     while ( 1 );
   }
 
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  
   if ( ! rtc.isrunning() ) {
     Serial.println( "RTC is NOT running!" );
     // following line sets the RTC to the date & time this sketch was compiled
@@ -63,8 +65,21 @@ void setup () {
 }
 
 void loop () {
+  
+  // Query RTC for a new datetime if var is set.
+  if ( query_datetime == 1 ) {
+    now = rtc.now();
+    query_datetime = 0;
+  }
 
-  now = rtc.now();
+  // Determine what kind of millis we do need to add to time. Could be current or current - previous.
+//  if ( ! previous_millis ) {
+//    now.AddMilliseconds( current_millis );
+//  }
+//  else {
+//    unsigned long millis_to_add = current_millis - previous_millis;
+//    now.AddMilliseconds( millis_to_add );
+//  }
   
   Serial.print(now.year());
   Serial.print('/');
@@ -131,4 +146,5 @@ void loop () {
     previous_output_green   = output_green;
   }
 
+  query_datetime = 1;
 }
